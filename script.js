df8313a477f2b69315c28a067bf91593987640b0
petrku <petr.kuznetsov@erv.ru>
2023-10-30 13:00:56 +0300

    Cleanup + style rework

--- script.js
+++ script.js
@@ -2,18 +2,13 @@ const API_KEY = '9gjvoXWa0NBklahjBs3QbNGnDsn7NnAUg6bqWktidPg';
 
 const content = document.querySelector('.content')
 
+const navbar = document.querySelector('.navBar');
+
 const searchButton = document.querySelector('#searchBar-button')
 
 const searchBar = document.querySelector('.searchBar-inputField')
 
 const noResultsMessage = document.querySelector('.noResultsMessage')
-
-const controls = document.querySelector(".controls");
-
-const prevPageButton = document.querySelector(".previousPage");
-const nextPageButton = document.querySelector(".nextPage");
-const currentPageCounter = document.querySelector(".currentPage")
-const totalPagesCounter = document.querySelector(".totalPages");
 
 const loader = document.querySelector('.loader');
 
@@ -21,6 +16,11 @@ const fullSize = document.querySelector('.modal')
 
 const closeModalButton = document.querySelector('.modal__close')
 
+let scrollListener = function () {
+    if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+        changePage(1);
+    }
+};
 document.body.addEventListener('keydown', (e)=>{
     if (e.key == 'Escape' && fullSize.classList.contains('open')){
         document.body.classList.remove('noScroll')
@@ -55,27 +55,13 @@ const LOADER = {
     show: "flex",
     hide: "none",
     position : {
-        loading_another_page: "50px",
-        deafult: "35vh",
-    }
-}
-
-const CONTROLS = {
-    hide: "controlsHide",
-    show: "controlsShow",
-    position : {
-        loading_another_page: "120px",
-        default: "0px",
+        deafult: "35vh"
     }
 }
 
 const NO_RESULT_MESSAGE = {
     hide: "none",
     show: "flex",
-}
-
-function getTotalPages(){
-    return +(totalPagesCounter.textContent.slice(2))
 }
 
 function createImage (url){
@@ -98,23 +84,20 @@ function createImage (url){
 async function getImages(query, page){
     currentQuery = query;
     currentPage = page;
-    content.innerHTML = CONTENT.clear;
+    navbar.classList.remove('navBarDefault');
     loader.style.display = LOADER.show;
     noResultsMessage.style.display = NO_RESULT_MESSAGE.hide;
     if (!page) {
-        currentPageCounter.value = 1;
         currentPage = 1;
-        controls.id = CONTROLS.hide;
     } else {
-        loader.style.top = LOADER.position.loading_another_page
-        controls.style.marginTop = CONTROLS.position.loading_another_page
+        loader.style.top = LOADER.position.deafult
     }
 
     const {data} = await axios.get(`https://api.unsplash.com/search/photos/`, {
         params : {
             query: `${query}`,
             page: page ?? 1,
-            per_page: 12,
+            per_page: 15,
         },
         headers : {
             "Authorization": `Client-ID ${API_KEY}`,
@@ -123,19 +106,13 @@ async function getImages(query, page){
 
     loader.style.top = LOADER.position.deafult;
     loader.style.display = LOADER.hide;
-    controls.style.marginTop = CONTROLS.position.default
-    controls.id = CONTROLS.show;
 
-    const totalPages = data.total_pages > 200 ? 200 : data.total_pages
-    totalPagesCounter.textContent = `/ ${totalPages}`
-    currentPageCounter.setAttribute('max', totalPages)
-
-    if (data.total === 0 && data.total_pages === 0) {
+    if (data.total === 0) {
         noResultsMessage.style.display = NO_RESULT_MESSAGE.show;
-        controls.id = CONTROLS.hide;
+        window.removeEventListener("scroll", scrollListener);
     } else {
+        window.addEventListener("scroll", scrollListener);
         noResultsMessage.style.display = NO_RESULT_MESSAGE.hide;
-        controls.id = CONTROLS.show
         for (const iterator of data.results) {
             content.appendChild(createImage(iterator.urls.regular))
         }
@@ -143,46 +120,16 @@ async function getImages(query, page){
 }
 
 searchButton.addEventListener('click', ()=>{
+    content.innerHTML = CONTENT.clear
     getImages(searchBar.value);
     searchBar.blur()
 });
 
 searchBar.addEventListener('keydown', (event)=>{
     if (event.keyCode === KEYBOARD.enter){
+        content.innerHTML = CONTENT.clear
         getImages(searchBar.value);
         searchBar.blur()
-    }
-})
-
-currentPageCounter.addEventListener('keydown', (event)=>{
-    if (event.keyCode === KEYBOARD.enter){
-        currentPageCounter.value = Number(currentPageCounter.value);
-        if (!currentPageCounter.value){
-            currentPageCounter.value = 1;
-        }
-        const targetPage = +currentPageCounter.value;
-        window.scroll({
-            top: 0, 
-            left: 0, 
-            behavior: 'smooth'
-          });
-        getImages(searchBar.value, targetPage);
-        currentPageCounter.blur();
-    }
-})
-
-currentPageCounter.addEventListener('input',() => {
-    let value = currentPageCounter.value;
-    if (isNaN( Number(value) ) || Number(value) === 0){
-        value = value.replace(/[^1-9]/g, "");
-    }
-    value = value > getTotalPages() ? getTotalPages() : value;
-    currentPageCounter.value = value;
-})
-
-currentPageCounter.addEventListener('blur', ()=>{
-    if (currentPageCounter.value == '') {
-        currentPageCounter.value = 1;
     }
 })
 
@@ -193,11 +140,5 @@ function changePage(value){
         targetPage = 1;
         currentPage = 1;
     }
-    currentPageCounter.value = targetPage;
-    window.scroll({
-        top: 0, 
-        left: 0, 
-        behavior: 'smooth'
-      });
-    setTimeout(()=>{getImages(currentQuery, targetPage)}, 750)
+    getImages(currentQuery, targetPage);
 }
